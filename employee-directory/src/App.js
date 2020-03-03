811d2ff612f67305fca37a0df7a9b2ce8f1f8a6f
Leah Barnes <johnson.leahe@gmail.com>
2020-03-02 20:57:53 -0500

    Adding header and header css.

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -4,3 +4,26 @@ import './App.css';
 // import employees from "./employees.json";
 import Searchbar from "./components/Searchbar";
 
+class App extends Component {
+  // state = {
+  //   employees
+  // };
+
+// filteremployee....by name
+
+render() {
+  return (
+<div className="container">
+<Header>
+  <h1>Employee Directory</h1>
+</Header>
+
+
+</div>
+  
+)
+  }
+
+}
+
+export default App;
