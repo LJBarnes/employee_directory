84dffaffffd87eb333b6214d5025126aac6b55a4
Leah Barnes <johnson.leahe@gmail.com>
2020-03-06 20:40:19 -0500

    Redid searchbar component. Search not working.

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,30 +1,79 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Header from "./components/Header";
 import './App.css';
-// import employees from "./employees.json";
+import employees from "./employees.json";
 import Searchbar from "./components/Searchbar";
+import Tbl from "./components/Tbl";
+
 
 class App extends Component {
-  // state = {
-  //   employees
-  // };
 
-// filteremployee....by name?
-
-render() {
-  return (
-<div className="container">
-<Header>
-  <h1>Employee Directory</h1>
-</Header>
-<Searchbar />
+  // setting initial state
+  state = {
+    employees,
+    search: ""
+  };
 
 
-</div>
+
+  // When the form is submitted, search the employees.json `this.state.search`
+  handleFormSubmit = event => {
+    event.preventDefault();
+    const filterEmp = employees.filter(emp => emp.name === this.state.search);
+    this.setState({ employees: filterEmp });
+    }
   
-)
-  }
 
+  handleInputChange = event => {
+    console.log(event.target.value);
+    this.setState({ search: event.target.value })
+  };
+
+ 
+
+
+  // function for sorting employees in alphabetical order
+
+
+  // we want to map over employees and render a row for each employee
+   render () {
+    return (
+      <div className="container">
+        <Header>
+          <h1>Employee Directory</h1>
+        </Header>
+
+        <Searchbar 
+        search={this.state.search}
+        handleFormSubmit={this.handleFormSubmit}
+        handleInputChange={this.handleInputChange}
+        />
+
+        <table>
+
+                <tr>
+                    <th>ID</th>
+                    <th>Pic</th>
+                    <th>Name <button type="button" className="btn btn-info">sort</button></th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                </tr>
+                </table>
+        {this.state.employees.map(employee => (
+        <Tbl
+          id ={employee.id}
+          key={employee.id}
+          name={employee.name}
+          image={employee.image}
+          phone={employee.phone}
+          email={employee.email}
+        />
+        ))}
+      </div>
+
+    );
+  }
 }
+
 
 export default App;
--- employee-directory/src/components/Searchbar/index.js
+++ employee-directory/src/components/Searchbar/index.js
@@ -1,51 +1,27 @@
 import React, { Component } from "react";
 import "./style.css";
 
-class Searchbar extends Component {
-  // Setting the component's initial state
-  state = {
-    employeeName: "",
-  };
 
-  handleInputChange = event => {
-    // Getting the value and name of the input which triggered the change
-    let value = event.target.value;
-    const name = event.target.name;
-
-    // Updating the input's state
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    event.preventDefault();
-   this.setState({
-      employeeName: "",
-      
-    });
-  };
-
-  render() {
-    // Notice how each input has a `value`, `name`, and `onChange` prop
-    return (
-      <div>
-        
-        <form className="form">
-          <input
-            value={this.state.employeeName}
-            name="employeeName"
-            onChange={this.handleInputChange}
-            type="text"
-            placeholder="Employee Name"
-          />
-          
-          <button onClick={this.handleFormSubmit}>Submit</button>
-        </form>
-      </div>
-    );
-  }
+function Searchbar(props) {
+  return (
+    <form>
+    <div className="form-group">
+      <input
+        value={props.search}
+        onChange={props.handleInputChange}
+        name="employee"
+        type="text"
+        className="form-control"
+        placeholder="Search for an employee by name"
+        id="search"
+      />
+      <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">
+        Search
+      </button>
+    </div>
+  </form>
+  )
 }
+
 
 export default Searchbar;
